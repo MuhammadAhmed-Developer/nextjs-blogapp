f11f225ee3b983801e3d0a8d4da75de3f886c0ec
MuhammadAhmed-Developer <ahmadginideveloper@gmail.com>
2023-11-26 16:09:52 +0500

    fix build

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,27 +1,31 @@
-import Link from 'next/link'
-import React from 'react'
-import { blogData } from '../constants/blog-data'
+import Link from "next/link";
+import React from "react";
+import { blogData } from "../constants/blog-data";
 
-  
 export default function Blogs() {
   return (
-    <section className=''>
-     <h1 className='text-5xl font-bold mt-8 text-center'> All Blogs</h1>
+    <section className="">
+      <h1 className="text-5xl font-bold mt-8 text-center"> All Blogs</h1>
       <div>
         <div>
-            {blogData.map((item, i:number )=>{
-                return(
-                    <Link key={i} href={`/blogs/${item.slug}`} className='border-gray-900 flex justify-center'>
-                        <ul className='text-2xl border  my-4 cursor-pointer'>
-                            <li>
-                            {i + 1} : {item.title}
-                            </li>
-                        </ul>
-                    </Link>
-                )
-            })}
+          {blogData.map((item, i: number) => {
+            return (
+              <div key={i}>
+                <Link
+                  href={`/blogs/${item.slug}`}
+                  className="border-gray-900 flex justify-center"
+                >
+                  <ul className="text-2xl border  my-4 cursor-pointer">
+                    <li>
+                      {i + 1} : {item.title}
+                    </li>
+                  </ul>
+                </Link>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
-  )
+  );
 }
